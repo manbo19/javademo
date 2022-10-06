2f196618a21b9be405b9b3e831438c9ca666dd3a
jeongmin <jeongmin0619@gmail.com>
2022-10-06 18:23:53 +0900

    20221006 | update

--- src/java003_statments/Java031_1_for.java
+++ src/java003_statments/Java031_1_for.java
@@ -5,20 +5,18 @@ public class Java031_1_for {
 	public static void main(String[] args) {
 		int data = 0;
 
-		label:
-		for (int row = 1; row <= 3; row++) {
+		label: for (int row = 1; row <= 3; row++) {
 			for (int col = 1; col <= 4; col++) {
-				//data++;
+				// data++;
 				System.out.printf("%4d", ++data);
-				
-				if(data%4==0) {
+
+				if (data % 4 == 0) {
 					System.out.println();
 					continue label;
 				}
-				
+
 			} // end for row
-			
-			
+
 		} // end for col
 	}// end main()
 
