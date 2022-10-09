757a3c269b8cb58ed9a52d88ab14bae770de3efb
min <jeongmin0619@gmail.com>
2022-10-09 13:38:20 +0900

    20221009(SUN.) | UPDATE

--- src/java004_array/Java037_array.java
+++ src/java004_array/Java037_array.java
@@ -10,36 +10,34 @@ public class Java037_array {
 		int ko = 90;
 		int en = 80;
 		int jp = 40;
-		
-		int[] arr; //배열 선언
-		//배열 생성 - 메모리 생성(확보)
-		//new 키워드를 이용해서 배열을 생성
-		//각 요소(element)마다 인덱스가 0부터 생성된다.
-		arr = new int[3]; //배열 생성
 
-		//int [] arr = new int [3]
-		
+		int[] arr; // 배열 선언
+		arr = new int[3]; // 배열 생성
+		// 배열 생성 - 메모리 생성(확보)
+		// new 키워드를 이용해서 배열을 생성
+		// 각 요소(element)마다 인덱스가 0부터 생성된다.
+		// int [] arr = new int [3]
+
 		System.out.println(arr[0]);
 		System.out.println(arr[1]);
 		System.out.println(arr[2]);
-		
+
 		arr[0] = 90;
 		arr[1] = 80;
 		arr[2] = 40;
 		System.out.println(arr[0]);
 		System.out.println(arr[1]);
 		System.out.println(arr[2]);
-		
-		
-		//Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
-		//System.out.println(arr[3]);
-		
-		//배열의 크기: length
-		for(int i=0; i<arr.length; i++) {
+
+		// Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3
+		// out of bounds for length 3
+		// System.out.println(arr[3]);
+
+		// 배열의 크기: length
+		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
-		
-		
-	}//end main()
 
-}//end class
+	}// end main()
+
+}// end class
