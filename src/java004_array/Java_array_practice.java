82c30ecc37bd88059f0fd9bce0ecc79370f9ed1f
min <jeongmin0619@gmail.com>
2022-10-09 15:46:03 +0900

    20221009(sun.) | UPDATE

---
+++ src/java004_array/Java_array_practice.java
@@ -0,0 +1,20 @@
+package java004_array;
+
+import java.util.Arrays;
+
+public class Java_array_practice {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 100, 95, 80, 70, 60 };
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+
+		}
+		System.out.println();
+		
+		char[] chArr = { 'a', 'b', 'c', 'd' };
+		System.out.println(chArr);
+
+	}
+
+}
