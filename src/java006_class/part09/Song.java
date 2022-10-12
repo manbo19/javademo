1c0ef3cbb2a84c6d61d6d34b795d6229e170160c
jeongmin <jeongmin0619@gmail.com>
2022-10-12 16:30:48 +0900

    20221012(Wed.)

--- src/java006_class/part09/Song.java
+++ src/java006_class/part09/Song.java
@@ -7,14 +7,13 @@ public class Song {
 	String[] composer;
 	int year;
 	int track;
-	
+
 	public Song() {
-		
+		this("Dancing Queen", "ABBA", "Arrival", new String[] { "Benny Andersson, bjohn Ulvaeus" }, 1997, 2);
 	}
 
 	public Song(String title, String artist, String album, String[] composer, int year, int track) {
-		//this("Dancing Queen", "ABBA", "Arrival", new String[] { "Benny Andersson, bjohn Ulvaeus" }, 1997,2);
-		
+
 		this.title = title;
 		this.artist = artist;
 		this.album = album;
@@ -22,11 +21,12 @@ public class Song {
 		this.year = year;
 		this.track = track;
 	}
-	
+
 	public void show() {
-		
+		// for (int i = 0; i < composer.length; i++) {
+		System.out.printf("노래제목:%s\n 가수:%s\n 앨범:%s\n 작곡가:%s\n 연도:%d\n 트랙:%d\n", title, artist, album, composer[0], year,
+				track);
+		// }
 	}
-	
-	
 
 }
