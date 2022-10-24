e6f5c1d0f1bfb44760f999126f32cd3bff4c6981
jeongmin <jeongmin0619@gmail.com>
2022-10-24 16:56:14 +0900

    20221024(Mon.)

--- src/java018_collection/Java199_User.java
+++ src/java018_collection/Java199_User.java
@@ -1,5 +1,20 @@
 package java018_collection;
 
+/*
+ * 1. 와일드 카드(?)를 제공하는 이유
+ * 	컬렉션에 저장되는 요소들이 특정한 객체로 정해진 상태에서 다른 객체형은
+ *  저장할 수 없는 상태였다. 하지만 때로는 기존과 같이 모든 객체들을
+ *  저장해야 할 때가 있다.
+ *  Vector v = new Vector();
+ *  v.add(new Integer(20));
+ *  v.add(new String("java")).;
+ *  
+ * 2. 와일드 카드의 구성
+ * 	<?>: 모든 객체자료형에 대한 배치를 의미한다.
+ *  <? super 객체자료형> : 명시된 객체자료형이나 객체자료형의 상위객체들의 배치를 의미한다.
+ *  <? extends 객체자료형> : 명시된 객체자료형이나 객체자료형으로부터 상속받은 하위 객체들의 배치를 의미한다.
+ */
+
 class Seoul {
 	public void showYou() {
 		System.out.println("seoul");
@@ -31,11 +46,11 @@ class Orange extends Fruit {
 
 }
 
-class Fruitbox<T> {
+class FruitBox<T> {
 	private T item;
 
-	public Fruitbox() {
-		// TODO Auto-generated constructor stub
+	public FruitBox() {
+
 	}
 
 	public void store(T item) {
@@ -50,8 +65,33 @@ class Fruitbox<T> {
 public class Java199_User {
 
 	public static void main(String[] args) {
+		FruitBox<Orange> fOrange = new FruitBox<Orange>();
+		fOrange.store(new Orange());
+		openAndAllBox(fOrange);
+		openAndExtendsBox(fOrange);
+		// openAndSuperbox(fOrange);
+
+		FruitBox<Seoul> fSeoul = new FruitBox<Seoul>();
+		fSeoul.store(new Seoul());
+		openAndAllBox(fSeoul);
+		// openAndExtendsBox(fSeoul);
+		openAndSuperbox(fSeoul);
 
 	}// end main()
+
+	public static void openAndAllBox(FruitBox<?> param) { // ? = Object
+		System.out.println(param.pullout());
+		((Seoul) param.pullout()).showYou();
+
+	}// end openAndAllBox()
+
+	public static void openAndExtendsBox(FruitBox<? extends Fruit> param) {
+		param.pullout().showYou();
+	}// end openAndExtendsBox()
+
+	public static void openAndSuperbox(FruitBox<? super Fruit> param) {
+		((Seoul) param.pullout()).showYou();
+	}// end openAndSuperbox()
 
 }
 // end class
